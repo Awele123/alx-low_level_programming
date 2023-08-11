c424b11bef742cbe16cba6800becef123315b8a2
awele123 <nwabuzorawele42@gmail.com>
2023-08-11 16:00:08 -0700

    alpahabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int b;
+
+for (b = 'a'; b <= 'z'; b++)
+putchar(b);
+putchar('\n');
+return (0);
+}
+
