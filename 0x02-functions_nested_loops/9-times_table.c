10ece0a2d760d59881fd91b127f8464a2356d382
awele123 <nwabuzorawele42@gmail.com>
2023-08-16 14:14:49 -0700

    nine

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main -function that prints the 9 times table, starting with 0
+ * times_table -function that prints the 9 times table, starting with 0
  * Return:empty output
  */
 
@@ -17,7 +17,7 @@ void times_table(void)
 	if (z > 9)
 	{
 	u = z % 10;
-	d = (z - d) / 10;
+	d = (z - u) / 10;
 	_putchar(44);
 	_putchar(32);
 	_putchar(d + '0');
