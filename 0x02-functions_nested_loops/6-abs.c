3a58c2651411fe881075b371a01503be898b5526
awele123 <nwabuzorawele42@gmail.com>
2023-08-16 09:18:34 -0700

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,13 +3,13 @@
 /**
  * _abs - Function to find the absolute value of an integar
  * @a: function parameter
- * Return: -a or a
+ * Return: absolute value of number or zero
  */
 
 int _abs(int c)
 
 {
-	if (c > 0)
+	if (c < 0)
 	{
 	int abs_val;
 
