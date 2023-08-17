64ff1343f6e6aec026b31d27201571e64a72bff6
awele123 <nwabuzorawele42@gmail.com>
2023-08-17 16:52:03 -0700

    fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -entry code
+ * Return: 0;
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 1 ; b <= 100 ; b++)
+	{
+		if ((b % 3 == 0) && (b % 5 == 0))
+			printf("%s", "FizzBuzz");
+		else if (b % 3 == 0)
+			printf("%s", "Fizz");
+		else if (b % 5 == 0)
+			printf("%s", "Buzz");
+		else
+			printf("%d", b);
+		if (b != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
+
