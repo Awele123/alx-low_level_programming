80a2817e2ce8b6d099123c358353195a976c48ab
awele123 <nwabuzorawele42@gmail.com>
2023-08-16 17:00:21 -0700

    sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -program finds and prints the sum of the even-valued terms
+ * Return: 0
+ */
+
+int main(void)
+{
+	int fib1 = 1, fib2 = 2, fib3;
+	int sum = 0;
+
+	while (fib2 <= 4000000)
+	{
+		if (fib2 % 2 == 0)
+		{
+			sum += fib2;
+		}
+		fib3 = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = fib3;
+	}
+	printf("Sum of even Fibonacci terms below 4,000,000: %d\n", sum);
+	return (0);
+}
