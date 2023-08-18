fa813ba42503f5fb33a3e5977d01901c04f7d6c8
awele123 <nwabuzorawele42@gmail.com>
2023-08-17 17:51:34 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,21 @@
 void print_diagonal(int n)
 {
 	if (n <= 0)
+	{
 		_putchar('\n');
-	else
+	} else
 	{
 		int a, b;
 
 		for (a = 0 ; a < n ; a++)
 		{
-			for (b = 0 ; b <= a ; b++)
-				_putchar(' ');
-			_putchar('\\');
+			for (b = 0 ; b < n; b++)
+			{
+				if (b == a)
+					_putchar('\\');
+				else if (b < a)
+					_putchar (' ');
+			}
 			_putchar('\n');
 		}
 	}
