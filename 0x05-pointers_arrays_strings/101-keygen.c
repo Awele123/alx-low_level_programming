d69f8b97b4c1c321715324cc1efbc9e82c15a8cb
awele123 <nwabuzorawele42@gmail.com>
2023-08-22 14:36:21 -0700

    pass

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,61 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
-#include <unistd.h>
 
 /**
- * main - generates random valid passwords for the program 101-crackme
+ * main -generates random valid passwords for the program 101-crackme
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int b;
+	int pass[100];
+	int d, zoom, e;
 
-	printf("length; ");
+	zoom = 0;
 
-	int length;
+	srand(time(NULL));
 
-	scanf("%d", &length);
-
-	if (length <= 0)
+	for (d = 0; d < 100; d++)
 	{
-		printf("Password length must be >= 1!");
-		return (1);
+		pass[d] = rand() % 78;
+		zoom += (pass[d] + '0');
+		putchar(pass[d] + '0');
+		if ((2883 - zoom) - '0' < 78)
+		{
+			e = 2772 - zoom - '0';
+			zoom += e;
+			putchar(e + '0');
+			break;
+		}
+		putchar('\n');
 	}
 
-	char *password = malloc(length + 1);
-
-	char *digits = "012345";
-	int digits_length = strlen(digits);
-
-	char *lowers = "abcdef";
-	int lowers_length = strlen(lowers);
-
-	char *uppers = "ABCDEF";
-	int uppers_length = strlen(uppers);
-
-	char *symbols = "!@#$%";
-	int symbols_length = strlen(symbols);
-
-	srand(time(NULL) * getpid ());
-
-	for (b = 0 ; b < length ; b++)
-	{
-		int char_type = rand() % 4;
-
-		if (char_type == 0)
-			password[b] = digits[rand() % digits_length];
-		else if (char_type == 1)
-			password[b] = lowers[rand() % lowers_length];
-		else if (char_type == 2)
-			password[b] = uppers[rand() % uppers_length];
-		else
-			password[b] = symbols[rand () % symbols_length];
-	}
-	password[length] = '\0';
-	printf("Password: %s\n", password);
-
-	free(password);
+	return (0);
 }
+
+
