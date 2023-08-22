aacdf51d71a03afa587826cd51dedad58a82c9a9
awele123 <nwabuzorawele42@gmail.com>
2023-08-21 18:40:14 -0700

    fear

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _puts - function that prints a string, followed by a  * @str: string
+ * _puts - function that prints a string, followed by a new line
+ * @str: string
  * Return: void
  */
 
