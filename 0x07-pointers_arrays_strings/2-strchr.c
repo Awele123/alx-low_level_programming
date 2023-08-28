54c98e6ef11642e111dd3563d78df24ba1074d19
awele123 <nwabuzorawele42@gmail.com>
2023-08-28 11:48:49 -0700

    strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in a string.
+ * @s: input value
+ * @c: input value
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int r = 0;
+
+	for (; s[r] >= '\0'; r++)
+	{
+		if (s[r] == c)
+			return (&s[r]);
+	}
+	return (0);
+}
