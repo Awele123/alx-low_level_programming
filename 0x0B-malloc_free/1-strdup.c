212118d04ac9adf334eed1a4baa85a37f4bfc353
awele123 <nwabuzorawele42@gmail.com>
2023-09-05 15:30:53 -0700

    allocated

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup -returns a pointer
+ * @str: char
+ * Return: result
+ */
+
+char *_strdup(char *str)
+{
+	char *win;
+	int y, j;
+
+	y = 0;
+	j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[y] == '\0')
+		y++;
+	win = malloc(sizeof(char) * (y + 1));
+	if (win == NULL)
+		return (NULL);
+	for (j = 0; str[j]; j++)
+		win[j] = str[j];
+	free(win);
+
+	return (win);
+}
