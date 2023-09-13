e239d8a47c9dfe089b4f975ed5ebb2312438fbcb
awele123 <nwabuzorawele42@gmail.com>
2023-09-13 10:42:02 -0700

    fun

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -40,16 +40,6 @@ int op_mul(int a, int b)
 {
 	return (a * b);
 }
-/*op_mod: prints remainder of the division of two numbers.
- * @a: number 1
- * @b: number 2
- * Return: remainder of the division of a by b.
- */
-
-int op_mod(int a, int b)
-{
-	return (a % b);
-}
 
 /**
  * op_div - prints the division of two numbers.
@@ -61,4 +51,16 @@ int op_mod(int a, int b)
 int op_div(int a, int b)
 {
 	return (a / b);
+}
+
+/**
+ * op_mod: prints remainder of the division of two numbers.
+ * @a: number 1
+ * @b: number 2
+ * Return: remainder of the division of a by b.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
