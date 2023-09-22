cd0563cd23b04fc7b9faf2d082e71cc2d0721d0d
awele123 <nwabuzorawele42@gmail.com>
2023-09-21 18:06:57 -0700

    third

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node -functiin tgat inserts a node to list_t
+ * @head: pointer to node A
+ * @str: string
+ * Return: result
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *edit;
+	unsigned int type = 0;
+
+	switch (str[type])
+	{
+		case '\0':
+			break;
+		default:
+			while (str[type])
+				type++;
+	edit = malloc(sizeof(list_t));
+	if (!edit)
+		return (NULL);
+
+	edit->str = strdup(str);
+	edit->len = type;
+	edit->next = (*head);
+	(*head) = edit;
+	}
+
+	return (*head);
+}
