e1671eabb000e6e7cc4ba5a580ecfe23d2ca9ecd
awele123 <nwabuzorawele42@gmail.com>
2023-09-21 18:34:33 -0700

    fourth

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - inserts node at end of list_t
+ * @head: pointer to node A
+ * @str: string
+ * Return: result
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *edit;
+	list_t *psudoe = *head;
+	unsigned int type = 0;
+
+	switch (str[0])
+	{
+		case '\0':
+			return NULL;
+		default:
+	for (type = 0; str[type]; type++)
+
+	edit = malloc(sizeof(list_t));
+	if (!edit)
+		return (NULL);
+
+	edit->str = strdup(str);
+	edit->len = type;
+	edit->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = edit;
+		return (edit);
+	}
+
+	while (psudoe->next)
+	       	psudoe = psudoe->next;
+	psudoe->next = edit;
+	}
+	return (edit);
+}
